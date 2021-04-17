cb040bfeac11270772537611e2339e245a2d6ecd
Ville Kurkela <kurkela.ville@hotmail.com>
2021-04-18 01:50:20 +0300

    add

---
+++ script.js
@@ -0,0 +1,25 @@
+window.onload = function() {
+	Stats();
+}
+
+function Stats() {
+	fetch('https://coronavirus-tracker-api.herokuapp.com/v2/locations/117')
+	.then(function(resp) { 
+        return resp.json() 
+    })
+	.then(function(data) {
+		var population = data.location.country_population;
+		var update = data.location.last_updated;
+		var confirmedCases = data.location.latest.confirmed;
+		var deaths = data.location.latest.deaths;
+
+		document.getElementById('vaesto').innerHTML = population.toLocaleString('en');
+		document.getElementById('paivitys').innerHTML = update.substr(0, 10);
+		document.getElementById('tapaukset').innerHTML = confirmedCases.toLocaleString('en');
+		document.getElementById('kuolemat').innerHTML = deaths.toLocaleString('en');
+	})
+	.catch(function() {
+		console.log("error");
+	})
+	setTimeout(Stats, 20000000)
+}
